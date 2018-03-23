ccd1b7a9268104898263471301d1c0e5d309b037
Shashi Shekhar <sshekhar@gmail.com>
2018-03-24 03:30:31 +0530

    Admin Portal

---
+++ module/Admin/src/Admin/Controller/AdminController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\Session\Container;
+use Admin\Model\AdminUser;
+
+class AdminController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function adminAuthAction(){
+        if ($this->getRequest()->isPost()) {
+            $sm = $this->getServiceLocator();
+            $userEmailID = $this->params()->fromPost('email');
+            $password = $this->params()->fromPost('password');             
+            $adminUser = new AdminUser();
+            $login = $adminUser->login($sm->get('dbAdapter'), $userEmailID, $password);
+            if($login){
+                $admin_session = new Container('eLearningAdmin');
+                $admin_session->emailID = $adminUser->getAdminEmailID();
+                $admin_session->userID = $adminUser->getAdminUserID();
+                return $this->redirect()->toRoute('adminPortal');
+            }else{
+                $this->flashMessenger()->addMessage(json_encode(array("status" => "invalid", "message" => 'Invalid Email or Password')));
+                return $this->redirect()->toRoute('adminPortalLogin');
+            }
+        }
+    }
+    
+    public function adminPortalLoginAction(){
+        $viewModel = new ViewModel();
+        $viewModel->setTerminal(true);
+        return $viewModel;
+    }
+
+
+}
+
